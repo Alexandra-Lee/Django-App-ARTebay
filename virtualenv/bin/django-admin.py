6eab3c3b59ff8a4fcfd5b9a4c63ec7fe3200e555
Alexandra LEE <palexalee@gmail.com>
2018-09-20 17:06:53 +0200

    "adding structure"

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexandralee/Rendu/Coding-Academy/Django_Start/virtualenv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ virtualenv/src/profiles/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(default='my description here')
+    location = models.CharField(max_length=120, default='my location is ...') 
+    occupation = models.CharField(max_length=120, null=True) 
+
+    def __unicode__(self):
+        return self.name +"      " + self.description + "  " + self.location
